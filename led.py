fdcc622895189d3f79677a9df4e2b11184f85d6f
ai-iot <hoge@etc.com>
2018-11-30 13:13:37 +0900

    add comment

--- led.py
+++ led.py
@@ -1,16 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIOピンをピン番号で指定
 GPIO.setmode(GPIO.BOARD)
 LED = 11
 
+# GPIO出力の設定
 GPIO.setup(LED, GPIO.OUT)
 
 for i in range(3):
+    #出力
     GPIO.output(LED, GPIO.HIGH)
+    #指定した時間だけ待機
     time.sleep(2)
 
     GPIO.output(LED, GPIO.LOW)
     time.sleep(1)
 
+#終了処理
 GPIO.cleanup()
--- temp.py
+++ temp.py
@@ -10,16 +10,17 @@ from matplotlib import pyplot as plt
 RANGE_XTIME = 60
 
 
-#h,t = dht.read_retry(dht.AM2302,4)
 h,t = dht.read_retry(dht.DHT11,4)
 
-
+# ---データ格納場所の用意---
 tempList = np.array([float(round(t,2)) for _ in range(RANGE_XTIME)])
 humdList = np.array([float(round(h,2)) for _ in range(RANGE_XTIME)])
+# ---データ格納場所の用意---
 
 loopCount = 0
 loopList = np.zeros(RANGE_XTIME)
 
+# ---グラフ描画の準備---
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 ax2 = ax1.twinx()
@@ -30,36 +31,45 @@ ax1.set_ylim(20,30)
 ax1.set_xlabel("time[s]")
 ax1.set_ylabel("Temperature")
 ax2.set_ylabel("Humidity")
+# ---グラフの準備---
 
 while True:
-#for i in range(5):
-    # time.sleep(0.1)
-    #h,t = dht.read_retry(dht.AM2302,4)
+    # 温湿度データの取得
     h,t = dht.read_retry(dht.DHT11,4)
 
+    # ---取得したデータをターミナルに表示---
     data = {
         "temperature":t,
         "humidity":h
     }
-
     print("send:{}".format(data))
+    # ---取得したデータをターミナルに表示---
 
+    # 温度データをグラフ描画のために保管
     if abs(t - tempList[-1]) < 20:
         tempList = np.append(tempList,float(round(t,2)))
     else:
         tempList = np.append(tempList,float(round(tempList[-1],2)))
+
+    # 一番古い温度データを削除
     tempList = np.delete(tempList,0)
 
+    # 湿度データをグラフ描画のために保管
     if abs(h - humdList[-1]) < 20:
         humdList = np.append(humdList,float(round(h,2)))
     else:
         humdList = np.append(humdList,float(round(humdList[-1],2)))
+
+    # 一番古い湿度データを削除
     humdList = np.delete(humdList,0)
 
+    # 取得時刻を保管
     loopList = np.append(loopList,float(loopCount))
+
+    # 削除した古い温湿度データの取得時刻を削除
     loopList = np.delete(loopList,0)
     
-    
+    # ---グラフに描画するための準備---
     lax1.set_data(loopList, tempList)
     lax2.set_data(loopList, humdList)
  
@@ -67,11 +77,12 @@ while True:
     ax1.set_ylim(min(tempList)-0.1,max(tempList)+0.1)
     ax2.set_ylim(min(humdList)-0.1,max(humdList)+0.1)
 
-
     h1,l1 = ax1.get_legend_handles_labels()
     h2,l2 = ax2.get_legend_handles_labels()
     ax1.legend(h1+h2,l1+l2,loc='best')
+    # ---グラフに描画するための準備---
 
+    # グラフに描画
     plt.pause(0.1)
     loopCount += 1
 
