74b37c630b0c2b02d5003c1117a05ef833575a37
GGBBB2000 <ggbbb@mineo.jp>
2019-11-24 16:23:14 +0900

    sample: サンプルの追加/更新

---
+++ sample/arithmetic.py
@@ -0,0 +1,7 @@
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+a **= 2 # a = a ** 2 と同等. aにaを2乗下値を代入している
+print(a / b)
+print(a % b)
--- sample/variable.py
+++ sample/variable.py
@@ -1,4 +1,6 @@
 a = 10 # 変数aを値10で初期化した
 
 print(a) # Ok
+a = 100  # 変数aに値を代入
+print(a) # 100が出力される
 print(b) # Error
